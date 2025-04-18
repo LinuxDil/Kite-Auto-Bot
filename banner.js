77f671db45fdd960320ace072aa587ec31543030
Jack Sparrow <133944655+LinuxDil@users.noreply.github.com>
2025-04-19 01:29:38 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+export const banner = `
+               ${chalk.yellow('KITE AI  AUTO - BOT')}                
+  ${chalk.yellow('Telegram Channel: https://t.me/airdropseeker_official')}`;
