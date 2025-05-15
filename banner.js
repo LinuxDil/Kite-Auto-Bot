fb04e3a915d88e6862503c70e4970802ec482bf1
Jack Sparrow <133944655+LinuxDil@users.noreply.github.com>
2025-05-16 03:18:45 +0700

    Update and rename banner.js to accounts.txt

--- banner.js
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-export const banner = `
-               ${chalk.yellow('KITE AI  AUTO - BOT')}                
-  ${chalk.yellow('Telegram Channel: https://t.me/airdropseeker_official')}`;
